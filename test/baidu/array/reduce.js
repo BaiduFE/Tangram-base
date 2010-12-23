372d5ce1ac3855e957a0e85afba1594ab5b64740
Cat Chen <cathsfz@gmail.com>
2010-12-23 22:12:11 +0800

    added array.reduce test cases

---
+++ test/baidu/array/reduce.js
@@ -0,0 +1,13 @@
+module("baidu.array.reduce");
+
+test("基础校验", function(){
+	var arr = [1,2,3], ite = function(acc, i){return acc+i;};
+	var sum = baidu.array.reduce(arr, ite, 0);
+	equals(sum, 6, "校验返回结果");
+});
+
+test("特殊情况", function(){
+	//空数组
+	var arr = [], ite = function(acc, i){return acc+1;};
+	equals(baidu.array.map(arr, ite, 0), 0, "空串转换应该返回初始值");
+});
