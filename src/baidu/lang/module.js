5b6ee7a108f4bb5dc028385dfc491eac4b29939a
leeight <leeight@gmail.com>
2011-01-05 13:56:32 +0800

    revert baidu.lang.module

--- src/baidu/lang/module.js
+++ src/baidu/lang/module.js
@@ -10,22 +10,34 @@
  * @author erik, berg
  * @name baidu.lang.module
  * @function
- * @grammar baidu.lang.module(name, module[, opt_owner])
+ * @grammar baidu.lang.module(name, module[, owner])
  * @param {string} name 需要创建的模块名.
  * @param {Any} module 需要创建的模块对象.
- * @param {Object=} opt_owner 模块创建的目标环境，默认为window.
+ * @param {Object} [owner] 模块创建的目标环境，默认为window.
  * @remark
  *
             从1.1.1开始，module方法会优先在当前作用域下寻找模块，如果无法找到，则寻找window下的模块
 
  * @meta standard
  */
-baidu.lang.module = function(name, module, opt_owner) {
+baidu.lang.module = function(name, module, owner) {
     var packages = name.split('.'),
         len = packages.length - 1,
         packageName,
-        i = 0
-        owner = opt_owner || window;
+        i = 0;
+
+    // 如果没有owner，找当前作用域，如果当前作用域没有此变量，在window创建
+    if (!owner) {
+        try {
+            if (!(new RegExp('^[a-zA-Z_\x24][a-zA-Z0-9_\x24]*\x24')).test(packages[0])) {
+                throw '';
+            }
+            owner = eval(packages[0]);
+            i = 1;
+        }catch (e) {
+            owner = window;
+        }
+    }
 
     for (; i < len; i++) {
         packageName = packages[i];
@@ -35,7 +47,7 @@ baidu.lang.module = function(name, module, opt_owner) {
         owner = owner[packageName];
     }
 
-    if(!owner[packages[len]]){
+    if (!owner[packages[len]]) {
         owner[packages[len]] = module;
     }
 };
