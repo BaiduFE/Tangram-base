5d9c2e1fc44c1dd3112963226e9d4bef2e53cced
bellcliff <cliff_bell.cn@hotmail.com>
2011-01-05 19:31:30 +0800

    update by bell 让ready用例中判断用例执行终止的逻辑清晰一些， 另外，简单的格式化代码

--- test/baidu/dom/ready.js
+++ test/baidu/dom/ready.js
@@ -1,37 +1,30 @@
 module('baidu.dom.ready')
 
-test('ready',function(){
+test('ready', function() {
 	expect(1);
 	var iframe = document.createElement('iframe');
 	document.body.appendChild(iframe);
 	stop();
-	iframe.src = UserAction.commonData['datadir']+'testReady.html';
-//	alert(iframe.src);
+	iframe.src = UserAction.commonData['datadir'] + 'testReady.html';
 	window.iframeReady = false;
+	var clear = function(handle) {
+		clearInterval(handle);
+		delete window.iframeReady;
+		document.body.removeChild(iframe);
+		start();
+	}
 	var count = 0;
-	/**通过判断
-	 * iframe.contenWindow&&iframe.contentWindow.document&&iframe.contentWindow.document.body
-	 * 不能保证iframe已经被加载完全**/
-	var handle = setInterval(function(){
-		if(window.iframeReady||count > 14){
-			clearInterval(handle);
-			ok(window.iframeReady,'iframe is ready');
-			document.body.removeChild(iframe);
-			start();
-		}
-		else if(count > 14){
-			clearInterval(handle);
-			ok(false,'fail to call ready--timeout');
-			document.body.removeChild(iframe);
-			start();
-		}
-		else count++;
-	},20)
-	
-//	setTimeout(function(){
-//		ok(window.iframeReady,'iframe is ready');
-//		start();
-//	},100);
-
+	/***************************************************************************
+	 * 加载的iframe会尝试更新当前窗口中的变量iframeReady，预计超时时间为1s
+	 **************************************************************************/
+	var handle = setInterval(function() {
+		if (window.iframeReady) {
+			ok(window.iframeReady, 'iframe is ready');
+			clear(handle);
+		} else if (count > 50) {
+			ok(window.iframeReady, 'time out wait for ready');
+			clear(handle);
+		} else
+			count++;
+	}, 20)
 })
-
