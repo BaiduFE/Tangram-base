80f899d4e33dab99acabff4f0ef395b5eb0a93eb
Rocy <rocy03@gmail.com>
2011-01-05 19:22:52 +0800

    by rocy

---
+++ src/baidu/fn/blank.js
@@ -0,0 +1,17 @@
+/*
+ * Tangram
+ * Copyright 2011 Baidu Inc. All rights reserved.
+ * 
+ * path: baidu/fn/blank.js
+ * author: rocy
+ * version: 1.0.0
+ * date: 2011/01/05
+ */
+
+///import baidu.fn;
+
+/**
+ * 空函数.
+ * 用于需要排除函数作用域链干扰的情况.
+ */
+baidu.fn.blank = function () {};
