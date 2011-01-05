0b8dd70bfe597d1737fac1f3b66c0dc0b59c4e8b
leeight <leeight@gmail.com>
2011-01-05 13:53:38 +0800

    更新callByServer，恢复对string类型的callback的处理逻辑

--- src/baidu/sio/callByServer.js
+++ src/baidu/sio/callByServer.js
@@ -10,6 +10,7 @@
 
 ///import baidu.sio;
 ///import baidu.lang.isFunction;
+///import baidu.lang.isString;
 ///import baidu.sio._removeScriptTag;
 
 /**
@@ -17,9 +18,9 @@
  * @name baidu.sio.callByServer
  * @function
  * @grammar baidu.sio.callByServer(url, callback, opt_options)
- * @param {string} url 加载数据的url
- * @param {Function|string} callback 服务器端调用的函数或函数名
- * @param {Object=} opt_options 加载数据时的选项
+ * @param {string} url 加载数据的url.
+ * @param {Function|string} callback 服务器端调用的函数或函数名.
+ * @param {Object=} opt_options 加载数据时的选项.
  * @config {string} [charset] script的字符集
  * @config {string} [queryField] 服务器端callback请求字段名，默认为callback
  * @remark
@@ -36,23 +37,26 @@ baidu.sio.callByServer = function(url, callback, opt_options) {
         charset = options['charset'],
         queryField = options['queryField'] || 'callback';
 
-    callbackImpl = baidu.lang.isFunction(callback) ? callback : window[callback];
-
-    // 不管callback是string还是function，都重写callback
-    callbackName = prefix + Math.floor(Math.random() * 2147483648).toString(36);
-    window[callbackName] = function() {
-        try {
-            if(baidu.lang.isFunction(callbackImpl)){
-                callbackImpl.apply(window, arguments);
+    if (baidu.lang.isFunction(callback)) {
+        callbackName = prefix + Math.floor(Math.random() * 2147483648).toString(36);
+        window[callbackName] = function() {
+            try {
+                callback.apply(window, arguments);
+                window[callbackName] = null;
+                delete window[callbackName];
+            } catch (exception) {
+                // ignore the exception
+            } finally {
+                baidu.sio._removeScriptTag(scr);
             }
-            delete window[callbackName];
-        } catch (exception) {
-            // ignore the exception
-        } finally {
-            baidu.sio._removeScriptTag(scr);
-            window[callbackName] = null;
-        }
-    };
+        };
+    } else if (baidu.lang.isString(callback)) {
+        // XXX 如果callback是一个字符串的话，就需要保证url是唯一的，不要去改变它
+        // TODO 当调用了callback之后，无法删除动态创建的script标签
+        callbackName = callback;
+    } else {
+        return;
+    }
 
     url = url.replace((new RegExp('(\\?|&)callback=[^&]*')), '\x241' + queryField + '=' + callbackName);
     if (url.search(new RegExp('(\\?|&)' + queryField + '=/')) < 0) {
