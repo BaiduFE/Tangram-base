802362a5582d8ae3000d03257944a382fadf12ff
liujinlong <liujinlong@baidu.com>
2011-01-07 11:07:35 +0800

    增加用例，验证不设置marginLeft时的情况

--- test/baidu/dom/setPosition.js
+++ test/baidu/dom/setPosition.js
@@ -7,7 +7,23 @@ test('基础用例', function() {
     document.body.appendChild(div);
     div.style.position = 'absolute';
     div.style.marginLeft = '200px';
-    div.style.marginTop = '200px';
+    div.style.marginTop = '200px'; 
+
+    baidu.dom.setPosition(div, {
+        top : 100,
+        left : 100
+    });
+
+    equal(baidu.dom.getStyle(div, "left"), '-100px', "应该减去margin-left的偏移");
+    equal(baidu.dom.getStyle(div, "top"), '-100px', "应该减去margin-top的偏移");
+});
+
+test('not set marginLeft value', function() {
+
+    var div = document.createElement('div');
+
+    document.body.appendChild(div);
+    div.style.position = 'absolute';
 
     baidu.dom.setPosition(div, {
         top : 100,
