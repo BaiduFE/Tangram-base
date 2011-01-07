bb7081e225a678fe21e1fde668caf58057f905f5
Rocy <rocy03@gmail.com>
2011-01-07 16:49:18 +0800

    统一baidu.q的返回参数,不再返回null

--- src/baidu/dom/q.js
+++ src/baidu/dom/q.js
@@ -17,7 +17,7 @@
  * @name baidu.dom.q
  * @function
  * @grammar baidu.dom.q(className[, element, tagName])
- * @param {string} className 元素的class，只能指定单一的class，如果为空字符串或者纯空白的字符串，返回null。
+ * @param {string} className 元素的class，只能指定单一的class，如果为空字符串或者纯空白的字符串，返回空数组。
  * @param {string|HTMLElement} [element] 开始搜索的元素，默认是document。
  * @param {string} [tagName] 要获取元素的标签名，如果没有值或者值为空字符串或者纯空白的字符串，表示不限制标签名。
  * @remark 不保证返回数组中DOM节点的顺序和文档中DOM节点的顺序一致。
@@ -25,7 +25,7 @@
  * @meta standard
  * @see baidu.dom.g
  *             
- * @returns {Array} 获取的元素集合，查找不到或className参数错误时返回null
+ * @returns {Array} 获取的元素集合，查找不到或className参数错误时返回空数组.
  */
 baidu.dom.q = function (className, element, tagName) {
     var result = [], 
@@ -33,7 +33,7 @@ baidu.dom.q = function (className, element, tagName) {
     len, i, elements, node;
 
     if (!(className = trim(className))) {
-        return null;
+        return result;
     }
     
     // 初始化element参数
