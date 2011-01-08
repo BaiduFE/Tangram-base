b84f08dd7dc24a54fffb5f08db20622406a96236
berg <cnberg@gmail.com>
2011-01-08 15:13:18 +0800

    增加array包下contains, empty, every, some四个方法

---
+++ src/baidu/array/some.js
@@ -0,0 +1,28 @@
+/*
+ * Tangram
+ * Copyright 2009 Baidu Inc. All rights reserved.
+ */
+
+///import baidu.array;
+
+/**
+ * 判断一个数组中是否有部分元素满足给定条件
+ * @name baidu.array.some
+ * @function
+ * @grammar baidu.array.some(source, iterator)
+ * @param {Array} source 需要判断的数组.
+ * @param {Function} iterator 判断函数
+ * @return {boolean} 判断结果.
+ * @see baidu.array.every
+ * @author berg
+ */
+baidu.array.some = function(source, iterator) {
+    var i = 0,
+        len = source.length;
+    for (;i<len;i++) {
+        if(iterator.call(source, source[i],i)){
+            return true;
+        }
+    }
+    return false;
+};
