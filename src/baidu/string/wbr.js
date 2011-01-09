9e69270859e77f0c61b65be9ea528458b43ece9c
berg <cnberg@gmail.com>
2011-01-10 00:53:32 +0800

    更改一个更好的办法，来自ppk。http://www.quirksmode.org/oddsandends/wbr.html

--- src/baidu/string/wbr.js
+++ src/baidu/string/wbr.js
@@ -20,7 +20,7 @@
  * 
 1.支持html标签、属性以及字符实体。<br>
 2.任意字符中间都会插入wbr标签，对于过长的文本，会造成dom节点元素增多，占用浏览器资源。
-3.在opera下，浏览器默认css不会为wbr加上样式，导致没有换行效果，可以在css中加上 wbr { display: inline-block; } 解决此问题
+3.在opera下，浏览器默认css不会为wbr加上样式，导致没有换行效果，可以在css中加上 wbr:after { content: "\00200B" } 解决此问题
 		
  *             
  * @returns {string} 添加软换行后的字符串
