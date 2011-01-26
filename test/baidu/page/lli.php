de782317a33e2d9e541a3647aedfd6012a26f45c
bellcliff <cliff_bell.cn@hotmail.com>
2011-01-26 19:09:07 +0800

    update case for 1.3.4

--- test/baidu/page/lli.php
+++ test/baidu/page/lli.php
@@ -1,12 +1,7 @@
 <?php
-
-if($_GET['type'] == 'img'){
-	header("Content-Type: image/png");
-	header("Cache-Control: no-cache; ALL: no-store;");
-	imagepng(imagecreatefrompng("test.png"));
-}else {
-	header("Content-Type: text/html; charset=utf-8");
-	print '<html>
+header("Cache-Control: no-cache; ALL: no-store;");
+header("Content-Type: text/html; charset=utf-8");
+print '<html>
 	<script type="text/javascript" src="../../tools/br/import.php?f=baidu.page.lazyLoadImage&fromframe=true"></script>
 	<script type="text/javascript">
 	baidu.page.lazyLoadImage({	
@@ -18,9 +13,8 @@ if($_GET['type'] == 'img'){
 	</script>
 <body>
 <div style="width: 250px; height: 250px;"></div>
-<div><img src="lli.php?type=img" style="width: 1920px; height: 1200px;" /></div>
+<div><img id="test_img" src="test.png" style="width: 1920px; height: 1200px;" /></div>
 </body>
 </html>';
-}
 ?>
 
