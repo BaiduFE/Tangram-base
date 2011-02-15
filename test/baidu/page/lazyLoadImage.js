d50732ee805d0d017c574c82e15ad00cd587fd94
bellcliff <cliff_bell.cn@hotmail.com>
2011-02-15 20:32:30 +0800

    更新用例

--- test/baidu/page/lazyLoadImage.js
+++ test/baidu/page/lazyLoadImage.js
@@ -1,22 +1,29 @@
 module("baidu.page.lazyLoadImage");
 
 test("base", function() {
+	if(ua.browser.ie){
+		ok(true, "IE下不干活……");
+	};
 	expect(4);
 	var iframe = document.createElement('iframe');
 	document.body.appendChild(iframe);
 	$(iframe).css('height', 200).css('width', 200);
 	stop();
-	iframe.src = upath + 'lli.php?type=html';
+	iframe.src = upath + 'lli.php';
+	var doc = function(){
+		return frames[0].document;
+	};
 	setTimeout(function() {
-		equals(iframe.contentDocument.getElementById("test_img").src, "",
+		equals(frames[0].document.getElementById("test_img").src, "",
 				"图片链接被替换为空");
-		iframe.contentWindow.scroll(0, 20);
-	}, 100);
+		frames[0].scroll(0, 20);
+	}, 500);
 	setTimeout(function() {
-		equals(iframe.contentDocument.getElementById("test_img").src, "",
+		equals(frames[0].document.getElementById("test_img").src, "",
 				"图片不显示时链接依然为空");
-		iframe.contentWindow.scroll(0, 200);
-		ok(iframe.contentDocument.getElementById("test_img").src
+		frames[0].scroll(0, 200);
+		ok(frames[0].document.getElementById("test_img").src
 				.indexOf("test.png") >= 0, "图片显示时链接更新");
-	}, 200);
+		start();
+	}, 1000);
 });
