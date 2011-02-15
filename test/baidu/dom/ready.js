c5bebadb1bc86ed1dd819fd1884e93efd7eaa564
bellcliff <cliff_bell.cn@hotmail.com>
2011-02-15 20:21:24 +0800

    ready应该支持在ready状态的回调函数执行

--- test/baidu/dom/ready.js
+++ test/baidu/dom/ready.js
@@ -1,30 +1,42 @@
-module('baidu.dom.ready')
+module('baidu.dom.ready');
 
-test('ready', function() {
-	expect(1);
-	var iframe = document.createElement('iframe');
-	document.body.appendChild(iframe);
+test('页面载入完毕后调用该方法？', function() {
 	stop();
-	iframe.src = UserAction.commonData['datadir'] + 'testReady.html';
-	window.iframeReady = false;
-	var clear = function(handle) {
-		clearInterval(handle);
-		delete window.iframeReady;
-		document.body.removeChild(iframe);
+	expect(1);
+	baidu.dom.ready(function() {
+		ok(true);
 		start();
-	}
-	var count = 0;
-	/***************************************************************************
-	 * 加载的iframe会尝试更新当前窗口中的变量iframeReady，预计超时时间为1s
-	 **************************************************************************/
-	var handle = setInterval(function() {
-		if (window.iframeReady) {
-			ok(window.iframeReady, 'iframe is ready');
-			clear(handle);
-		} else if (count > 50) {
-			ok(window.iframeReady, 'time out wait for ready');
-			clear(handle);
-		} else
-			count++;
-	}, 20)
+	});
+});
+
+test('dom ready for none ie', function() {
+	if (!ua.browser.ie) {
+		expect(1);
+		window.iframeReady = false;// 子窗口中通过baidu.dom.ready尝试改变这个值用于确认函数是否正确执行
+		var iframe = document.createElement('iframe');
+		document.body.appendChild(iframe);
+		stop();
+		iframe.src = UserAction.commonData['datadir'] + 'testReady.html';// 文件位置在test/tools/data下
+		var clear = function(handle) {
+			clearInterval(handle);
+			// delete window.iframeReady;
+			// document.body.removeChild(iframe);
+			start();
+		}
+		var count = 0;
+		/***********************************************************************
+		 * 加载的iframe会尝试更新当前窗口中的变量iframeReady，预计超时时间为5s
+		 **********************************************************************/
+		var handle = setInterval(function() {
+			if (window.iframeReady) {
+				ok(window.iframeReady, 'iframe is ready');
+				clear(handle);
+			} else if (count > 250) {
+				ok(window.iframeReady, 'time out wait for ready');
+				clear(handle);
+			} else
+				count++;
+		}, 20)
+	} else
+		ok(true, 'IE下不支持子窗口');
 })
