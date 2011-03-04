bad15e349aec5286988e0f3e70fd392102a15968
bellcliff <cliff_bell.cn@hotmail.com>
2011-03-04 19:47:58 +0800

    update for mail list

--- test/tools/br/smail.php
+++ test/tools/br/smail.php
@@ -3,7 +3,9 @@ function sendmail($body, $debug = false){
 	include('Mail.php');
 	$headers['From']    = 'yangbo@baidu.com';
 	$headers['To']      = 'yangbo <yangbo@baidu.com>';
-//	$headers['Cc']      = 'liujinlong <liujinlong@baidu.com>, berg <leizhixing@baidu.com>, huangtiantian <huangtiantian@baidu.com>';
+	$headers['Cc']      = 'liujinlong <liujinlong@baidu.com>, '
+		.'berg <leizhixing@baidu.com>, huangtiantian <huangtiantian@baidu.com>';
+//		.'wangpengcheng <wangpengcheng@baidu.com>, lixiaopeng<lixiaopeng@baidu.com>';
 	$headers['Subject'] = '批量运行结果——tangram base';
 	$params['host'] = 'hotswap-c.baidu.com';//email.baidu.com';
 	$headers['Content-type'] = "text/html;charset=utf-8";//设置邮件内容为html格式
