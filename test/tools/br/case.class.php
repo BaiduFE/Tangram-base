e402721c04933a801032c7a7e72d242a9a064565
bellcliff <cliff_bell.cn@hotmail.com>
2011-03-17 16:10:42 +0800

    1、使可以通过中键通过弹出窗口打开用例执行 2、补充mouseenter两个关于body上挂在的用例，一个是html、一个是null

--- test/tools/br/case.class.php
+++ test/tools/br/case.class.php
@@ -159,7 +159,7 @@ class Kiss{
 			if($c->empty)
 			continue;
 			if($c->match($matcher)){
-				print("<a href=\"javascript:void(0)\" id=\"$c->case_id\" class=\"jsframe_qunit\" target=\"_blank\" title=\"$name\" onclick=\"run('$name');\$('#id_rerun').html('$name');return false;\">"
+				print("<a href=\"run.php?case=$name\" id=\"$c->case_id\" class=\"jsframe_qunit\" target=\"_blank\" title=\"$name\" onclick=\"run('$name');\$('#id_rerun').html('$name');return false;\">"
 				/*过长的时候屏蔽超出20的部分，因为隐藏的处理，所有用例不能直接使用标签a中的innerHTML，而应该使用title*/
 				.substr($name, 6)."</a>\n");
 			}
