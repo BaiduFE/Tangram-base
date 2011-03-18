85ec3a11533d54e70b2a28b47b317e212a9d561b
bellcliff <cliff_bell.cn@hotmail.com>
2011-03-18 14:02:11 +0800

    update for case

--- test/baidu/page/a.php
+++ test/baidu/page/a.php
@@ -2,10 +2,10 @@
 $type = $_GET['type'];
 $file = $_GET['file'];
 if($type == 'js'){
-	header("Content-type: text/javascript; charset=utf-8");
+	header("Content-type: text/html; charset=utf-8");
 }else if($type == 'css'){
 	header("Content-type: text/css; charset=utf-8");
 }else{
 	header("Content-type: text/plain; charset=utf-8");
 }
-return file_get_contents($file);
+echo file_get_contents($file);
