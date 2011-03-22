17da9f6f1422450ca83a6edb74addcac4df0089b
bellcliff <cliff_bell.cn@hotmail.com>
2011-03-22 17:18:29 +0800

    更新clone 用例

--- test/baidu/object/clone.js
+++ test/baidu/object/clone.js
@@ -42,7 +42,7 @@ test("clone函数输入number", function(){
 	ntwin = ntwin+2;
 	equals(n, 3, "n = 3");
 	equals(ntwin, 5, "ntwin = 5");
-})
+});
 
 test("clone函数输入number对象实例", function(){
 	var n;
@@ -185,131 +185,21 @@ test("clone函数输入的对象当中包含了Undefined", function(){
 	equals(o.n==otwin.o, true, "Undefined: o.n==otwin.o");
 });
 
+test('没有hasOwnProperty的情况', function(){
+	ua.frameExt(function(w){
+		var data = {};
+		data.imgsrc = w.document.createElement("IMG");
+		data.imgLoadStart = (new Date()).getTime();
+		data.imgsrc.src = upath+'img.jpg';
+		var data1 = baidu.object.clone(data);
+		equals(data1.imgsrc.src, data.imgsrc.src);
+		clearTimeout(h);
+		this.finish();
+	});//这个用例会执行超时。。。
+	var h = setTimeout(function(){
+		clearTimeout(h);
+		ok(false, '貌似又超时了……');
+		start();
+	}, 2000);
+});
 
-////baidu.object.clone的测试
-//describe("baidu.object.clone测试",{
-//    "clone函数输入string对象实例,用例1":function() {
-//        s = 'test';
-//        stwin = baidu.object.clone(s);
-//        value_of(stwin).should_be('test');
-//        stwin = stwin+'tail';
-//        value_of(s).should_be('test');
-//		value_of(stwin).should_be('testtail');
-//    },
-//    
-//    "clone函数输入string对象实例，用例2":function() {
-//        s = new String('test');
-//        stwin = baidu.object.clone(s);
-//        value_of(stwin).should_be('test');
-//        stwin = stwin+'tail';
-//        value_of(s).should_be('test');
-//		value_of(stwin).should_be('testtail');
-//    },
-//    "clone函数输入number":function (){
-//        n = 3.14;
-//        ntwin = baidu.object.clone(n);
-//        value_of(ntwin).should_be(3.14);
-//        ntwin = ntwin +2;
-//        value_of(n).should_be(3.14);
-////		value_of(ntwin).should_be(5.14);
-//    },
-//    "clone函数输入number对象实例":function (){
-//        n = new Number(3.14);
-//        ntwin = baidu.object.clone(n);
-//        value_of(ntwin).should_be(3.14);
-//        ntwin = ntwin +2;
-//        value_of(n).should_be(3.14);
-//	//	value_of(ntwin).should_be(5.14);
-//    },
-//    "clone函数输入object对象实例":function (){
-//        o = { a:1,b:'test',c:true};
-//        otwin = baidu.object.clone(o);
-//        value_of(o.a==otwin.a).should_be_true();
-//        value_of(o.b==otwin.b).should_be_true();
-//        value_of(otwin.c).should_be_true();
-//        otwin.a = 10; 
-//        otwin.b += 'tail';
-//		otwin.c = false;
-//        value_of(o.a).should_be(1);
-//        value_of(o.b).should_be('test');
-//        value_of(o.c).should_be_true();
-//		value_of(otwin.a).should_be(10);
-//        value_of(otwin.b).should_be('testtail');
-//        value_of(otwin.c).should_be_false();
-//		
-//    },
-//    "clone函数输入空对象":function (){
-//        empty = {};
-//        emptyTwin = baidu.object.clone(empty);
-//        value_of(empty.a).should_be_undefined();
-//        value_of(emptyTwin.a).should_be_undefined();
-//        emptyTwin.a = 'test';
-//        value_of(empty.a).should_be_undefined();
-//        value_of(emptyTwin.a).should_be('test');
-//    },
-//    "clone函数输入array":function (){
-//        array = [1,'test',3.14];
-//        arrayTwin = baidu.object.clone(array);
-//        value_of(array===arrayTwin).should_be_false();
-//        value_of(array[0]==arrayTwin[0]).should_be_true();
-//        value_of(array[1]==arrayTwin[1]).should_be_true();
-//        value_of(array[2]==arrayTwin[2]).should_be_true();
-//
-//        arrayTwin[0] = 100;
-//        value_of(array[0]).should_be(1);
-//		value_of(arrayTwin[0]).should_be(100);
-//    },
-//    "clone函数输入array中含有array":function (){
-//        var array = [[0,1], {'a':'3', 'b':['test']}, [1, [[5, 6], 4]]];
-//        var arrayTwin = baidu.object.clone(array);
-//        value_of(array===arrayTwin).should_be_false();
-//        value_of(array[0]==arrayTwin[0]).should_be_false();
-//		value_of(array[0][0] == arrayTwin[0][0]).should_be_true();
-//		value_of(array[0][1] == arrayTwin[0][1]).should_be_true();
-//		
-//		value_of(array[1]==arrayTwin[1]).should_be_false();
-//		value_of(array[1].a==arrayTwin[1].a).should_be_true();
-//		value_of(array[1].b==arrayTwin[1].b).should_be_false();
-//		value_of(array[1].b[0]==arrayTwin[1].b[0]).should_be_true();
-//		
-//        value_of(array[2]==arrayTwin[2]).should_be_false();
-//		value_of(array[2][0]==arrayTwin[2][0]).should_be_true();
-//		value_of(array[2][1]==arrayTwin[2][1]).should_be_false();
-//		value_of(array[2][1][0]==arrayTwin[2][1][0]).should_be_false();
-//		value_of(array[2][1][0][0]==arrayTwin[2][1][0][0]).should_be_true();
-//		value_of(array[2][1][0][1]==arrayTwin[2][1][0][1]).should_be_true();
-//		value_of(array[2][1][1]==arrayTwin[2][1][1]).should_be_true();
-//		
-//    },
-//    "输入的对象当中包含了方法":function (){
-//        o = new Object();
-//        o.a =1;
-//        o.b='test';
-//        o.c = true;
-//        o.fn = function (num){ value_of(this.a == num).should_be_true();};
-//        otwin = baidu.object.clone(o);
-//        value_of(o.a==otwin.a).should_be_true();
-//        value_of(o.b==otwin.b).should_be_true();            
-//        otwin.a =100;
-//        o.fn(1);
-//        otwin.fn(100);
-//    },
-//    "输入的对象当中包含了Date":function (){
-//        o = new Object();
-//        o.d = new Date();
-//        otwin = baidu.object.clone(o);
-//        value_of(o.d==otwin.d).should_be_true();
-//    },
-//    "输入的对象当中包含了Null":function (){
-//        o = new Object();
-//        o.n = null;
-//        otwin = baidu.object.clone(o);
-//        value_of(o.n===otwin.n).should_be_true();
-//    },
-//    "输入的对象当中包含了Undefined":function (){
-//        o = new Object();
-//        o.u;
-//        otwin = baidu.object.clone(o);
-//        value_of(o.u===otwin.u).should_be_true();
-//    }
-//});
