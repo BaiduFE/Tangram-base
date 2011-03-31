3d2b6225a9cfdc63f7fdc90459ac27dc675bd45f
bellcliff <cliff_bell.cn@hotmail.com>
2011-04-01 00:58:18 +0800

    update for maxthon

--- test/baidu/browser/maxthon.js
+++ test/baidu/browser/maxthon.js
@@ -2,9 +2,12 @@ module("baidu.browser.maxthon");
 
 test("maxthon", function() {
 	    try{
+	    	//遨游三这个地方改掉了……
 	        window.external.max_invoke("GetHotKey");
 	        ok(baidu.browser.maxthon, 'is maxthon');
 	    }catch(ex){
+	    	if(baidu.browser.maxthon == 3)
+	    		ok(true, '傲游3居然也抛异常……');
 	        equals(baidu.browser.maxthon, undefined, 'not maxthon');
 	    }
 });
