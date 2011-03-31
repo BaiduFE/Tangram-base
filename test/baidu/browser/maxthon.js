dfa127cccac8552178a5cc13d6fe841a858fd158
bellcliff <cliff_bell.cn@hotmail.com>
2011-04-01 02:10:57 +0800

    update for maxthon

--- test/baidu/browser/maxthon.js
+++ test/baidu/browser/maxthon.js
@@ -1,14 +1,15 @@
 module("baidu.browser.maxthon");
 
 test("maxthon", function() {
+	if (baidu.browser.maxthon >= 3){
+		ok(true, '傲游3居然也抛异常……');
+		return;
+	}
 	try {
 		// 遨游三这个地方改掉了……
 		window.external.max_invoke("GetHotKey");
 		ok(baidu.browser.maxthon, 'is maxthon');
 	} catch (ex) {
-		if (baidu.browser.maxthon >= 3)
-			ok(true, '傲游3居然也抛异常……');
-		else
 			equals(baidu.browser.maxthon, undefined, 'not maxthon');
 	}
 });
