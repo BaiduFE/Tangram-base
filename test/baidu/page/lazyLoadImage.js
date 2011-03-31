d4e81f66b96812db1a2d61de660151a689aba17d
bellcliff <cliff_bell.cn@hotmail.com>
2011-04-01 01:55:49 +0800

    udpate for case lazyLoadImage

--- test/baidu/page/lazyLoadImage.js
+++ test/baidu/page/lazyLoadImage.js
@@ -1,38 +1,40 @@
 module("baidu.page.lazyLoadImage");
 
 test("base", function() {
-	// if(ua.browser.ie){
-	// ok(true, "IE下不干活……");
-	// return;
-	// };
 	expect(3);
-
 	var iframe = document.createElement('iframe');
 	document.body.appendChild(iframe);
 	$(iframe).css('height', 200).css('width', 200);
 	stop();
-	iframe.src = upath + 'lli.php';
+	iframe.src = upath + 'lli.php?time=' + new Date().getTime();
 	var h = setInterval(function() {
 		if (frames[0] && frames[0].document
 				&& frames[0].document.getElementById("test_img")
 				&& frames[0].document.getElementById("test_img").src == '') {
+			var img = frames[0].document.getElementById("test_img");
 			// 标签就位，
 			clearInterval(h);
-			var step = 0;
-			baidu.on(frames[0], 'scroll', function() {
+			var step = 0,
+
+			check = function() {
 				if (step == 0) {
-					ok(this.document.getElementById("test_img").src
-							.indexOf("test.jpg") == -1, "图片不会显示");
+					ok(img.src.indexOf("test.jpg") == -1, "图片不会显示");
 					step = 1;
-					frames[0].scroll(0, 200);					
+					setTimeout(function() {// 这地方必须等，safari上不能连续滚
+						frames[0].scroll(0, 1000);
+					}, 0);
 				} else {
-					ok(this.document.getElementById("test_img").src
-							.indexOf("test.jpg") >= 0, "图片显示链接更新");
-					start();
+					baidu.un(frames[0], 'scroll', check);
+					setTimeout(function() {
+						ok(img.src.indexOf("test.jpg") >= 0, "图片显示链接更新");
+						iframe.parentNode.removeChild(iframe);
+						start();
+					}, 0);
 				}
-			});
+			};
+
+			baidu.on(frames[0], 'scroll', check);
 			frames[0].scroll(0, 20);
 		}
 	}, 50);
-
 });
