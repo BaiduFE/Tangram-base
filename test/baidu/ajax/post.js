b28ccd5b271415f8f191d84ca45b160320a6c6c5
bellcliff <cliff_bell.cn@hotmail.com>
2011-03-31 09:56:16 +0800

    这个用例我有明确印象之前改过....

--- test/baidu/ajax/post.js
+++ test/baidu/ajax/post.js
@@ -1,25 +1,5 @@
 module("baidu.ajax.post");
 
-test("正确url和正确data", function() {
-	stop();
-	expect(1);
-	var asyncDelay = 10000;
-	var urlstring = upath + "post.php";
-	var arg = "var1=baidu&var2=tangram";
-	var xhr = baidu.ajax.post(urlstring, arg);
-	var check = function() {
-		// IE下面遇到了"完成该操作所需的数据还不可使用。"的错误
-		return xhr.responseText && xhr.responseText.length > 0;
-	}, onsuccess = function() {
-		equals(xhr.responseText, "baidutangram", "xhr return");
-		start();
-	}, onfail = function() {
-		fail('timeout wait for reponse text in 10 seconds');
-		start();
-	};
-	ua.delayhelper(check, onsuccess, onfail);
-});
-
 test("onsuccess", function() {
 	stop();
 	expect(2);
@@ -47,3 +27,6 @@ test("输入不存在url以及设定onsuccess事件", function() {
 		start();
 	});
 });
+
+//TODO 大数据量提交校验
+//TODO 关于xml类型数据的交互
