d22cdbd6bfee9ceb4dbeb9d92ba50a2ae688954c
berg <cnberg@gmail.com>
2011-04-11 20:03:59 +0800

    sio.callByServer支持读取url中的callback参数做为回调方法名 ajax.form默认使用GET提交表单（同W3C规范） 修复swf.createHTML中如果flashvars中有逻辑假的值时，无法生成HTML属性的bug

--- src/baidu/sio/callByServer.js
+++ src/baidu/sio/callByServer.js
@@ -5,6 +5,7 @@
 
 ///import baidu.sio;
 ///import baidu.lang.isFunction;
+///import baidu.lang.isString;
 ///import baidu.sio._createScriptTag;
 ///import baidu.sio._removeScriptTag;
 
@@ -12,16 +13,16 @@
  * 通过script标签加载数据，加载完成由服务器端触发回调
  * @name baidu.sio.callByServer
  * @function
- * @grammar baidu.sio.callByServer(url, callback, opt_options)
+ * @grammar baidu.sio.callByServer(url, callback[, opt_options])
  * @param {string} url 加载数据的url.
- * @param {Function|string} callback 服务器端调用的函数或函数名.
+ * @param {Function|string} callback 服务器端调用的函数或函数名。如果没有指定本参数，将在URL中寻找options['queryField']做为callback的方法名.
  * @param {Object} opt_options 加载数据时的选项.
  * @config {string} [charset] script的字符集
  * @config {string} [queryField] 服务器端callback请求字段名，默认为callback
  * @config {Integer} [timeOut] 超时时间(单位：ms)，超过这个时间将不再响应本请求，并触发onfailure函数
  * @config {Function} [onfailure] timeOut设定后才生效，到达超时时间时触发本函数
  * @remark
- * 如果url中已经包含key为“callback”的query项，将会被替换成callback中参数传递或自动生成的函数名。
+ * 如果url中已经包含key为“options['queryField']”的query项，将会被替换成callback中参数传递或自动生成的函数名。
  * @meta standard
  * @see baidu.sio.callByBrowser
  */
@@ -34,31 +35,37 @@ baidu.sio.callByServer = function(url, callback, opt_options) {
         charset = options['charset'],
         queryField = options['queryField'] || 'callback',
         timeOut = options['timeOut'] || 0,
-        timer,reg;
-
-    if(!callback){
-        if(reg = new RegExp('(\\?|&)callback=([^&]*)', 'gi').exec(url)){
-            callback = reg[2];
-        }else{
-            callback = new Function();
-        }
-    }
+        timer,
+        reg = new RegExp('(\\?|&)' + queryField + '=([^&]*)'),
+        matches;
 
     if (baidu.lang.isFunction(callback)) {
         callbackName = prefix + Math.floor(Math.random() * 2147483648).toString(36);
         window[callbackName] = getCallBack(0);
-    } else {
-        // XXX 如果callback是一个字符串的话，就需要保证url是唯一的，不要去改变它
+    } else if(baidu.lang.isString(callback)){
+        // 如果callback是一个字符串的话，就需要保证url是唯一的，不要去改变它
         // TODO 当调用了callback之后，无法删除动态创建的script标签
         callbackName = callback;
+    } else {
+        if (matches = reg.exec(url)) {
+            callbackName = matches[2];
+        }
     }
 
     if( timeOut ){
         timer = setTimeout(getCallBack(1), timeOut);
     }
 
+    //如果用户在URL中已有callback，用参数传入的callback替换之
+    url = url.replace(reg, '\x241' + queryField + '=' + callbackName);
+    
+    if (url.search(reg) < 0) {
+        url += (url.indexOf('?') < 0 ? '?' : '&') + queryField + '=' + callbackName;
+    }
+    baidu.sio._createScriptTag(scr, url, charset);
+
     /*
-     * 返回一个函数，用于正常（挂在window上）或者超时（挂在setTimeout中）时执行
+     * 返回一个函数，用于立即（挂在window上）或者超时（挂在setTimeout中）时执行
      */
     function getCallBack(onTimeOut){
         /*global callbackName, callback, scr, options;*/
@@ -79,11 +86,4 @@ baidu.sio.callByServer = function(url, callback, opt_options) {
             }
         }
     }
-
-
-    url = url.replace((new RegExp('(\\?|&)callback=[^&]*')), '\x241' + queryField + '=' + callbackName);
-    if (url.search(new RegExp('(\\?|&)' + queryField + '=/')) < 0) {
-        url += (url.indexOf('?') < 0 ? '?' : '&') + queryField + '=' + callbackName;
-    }
-    baidu.sio._createScriptTag(scr, url, charset);
 };
