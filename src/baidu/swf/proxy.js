f2ffc674b2583dd50f09f06368491ee521b85a0f
rocy <rocy03@gmail.com>
2011-04-18 16:48:29 +0800

    添加 baidu.swf.Proxy from leeight.

--- src/baidu/swf/proxy.js
+++ src/baidu/swf/proxy.js
@@ -1,100 +1,88 @@
 /*
  * Tangram
  * Copyright 2011 Baidu Inc. All rights reserved.
- *
- * path: baidu/swf.js
- * author: liyubei@baidu.com (leeight)
- * version: 1.0.0
- * date: 2011/02/28
  */
 
 
 ///import baidu.swf;
 ///import baidu.swf.getMovie;
+///import baidu.lang.createClass;
 
 /**
- * baidu.swf.proxy
+ * baidu.swf.Proxy
+ * @name baidu.swf.Proxy
  * @constructor
- * @param {string} id 使用baidu.swf.create的时候指定的那个id.
- * @param {string} property 方法或者属性名称，用来检测Flash是否初始化好了.
+ * @param {string} id Flash的元素id.object标签id, embed标签name.
+ * @param {string} property Flash的方法或者属性名称，用来检测Flash是否初始化好了.
  * @param {Function=} opt_loadedHandler 初始化之后的回调函数.
+ * @remark Flash对应的DOM元素必须已经存在, 否则抛错. 可以使用baidu.swf.create预先创建Flash对应的DOM元素.
+ * @author liyubei@baidu.com (leeight)
  */
-baidu.swf.proxy = function(id, property, opt_loadedHandler) {
+baidu.swf.Proxy = baidu.lang.createClass(function(id, property, opt_loadedHandler) {
     this._id = id;
     this._property = property;
     this._loadedHandler = opt_loadedHandler || null;
-
+    this.initialized = false;
     /**
      * 页面上的Flash对象
      * @type {HTMLElement}
      */
     this._flash = null;
-};
-
-/**
- * @return {HTMLElement} Flash对象.
- */
-baidu.swf.proxy.prototype.getFlash = function() {
-    return (this._flash || (this._flash = baidu.swf.getMovie(this._id)));
-};
-
-/**
- * 初始化
- */
-baidu.swf.proxy.prototype.init = function() {
-    if (this._property) {
+    this._init();
+}).extend({
+    /**
+     * 获取flash对象.
+     * @return {HTMLElement} Flash对象.
+     */
+    getFlash : function() {
+        return (this._flash || (this._flash = baidu.swf.getMovie(this._id)));
+    },
+    /**
+     * 初始化
+     * @private
+     */
+    _init : function() {
+        if (! this._property) {
+            return;
+        }
         var flash = this.getFlash(),
             property = this._property,
-            loadedHandler = this._loadedHandler;
-        var timer = setInterval(function() {
-            try {
-                /** @preserveTry */
-                if (flash[property]) {
-                    clearInterval(timer);
-                    if (loadedHandler) {
-                        loadedHandler();
+            loadedHandler = this._loadedHandler,
+            timer = setInterval(function() {
+                try {
+                    /** @preserveTry */
+                    if (flash[property]) {
+                        this.initialized = true;
+                        clearInterval(timer);
+                        if (loadedHandler) {
+                            loadedHandler();
+                        }
                     }
+                } catch (e) {
                 }
-            } catch (e) {
+            }, 100);
+    },
+    /**
+     * 判断Flash是否初始化完成,可以与js进行交互.
+     */
+    isReady : function(){
+        return this.initialized;  
+    },
+    /**
+     * 调用Flash中的某个方法
+     * @param {string} methodName 方法名.
+     * @param {...*} var_args 方法的参数.
+     */
+    call : function(methodName, var_args) {
+        try {
+            var flash = this.getFlash(),
+                args = Array.prototype.slice.call(arguments);
+
+            args.shift();
+            if (flash[methodName]) {
+                flash[methodName].apply(flash, args);
             }
-        }, 100);
-    }
-};
-
-/**
- * 调用Flash中的某个方法
- * @param {string} methodName 方法名.
- * @param {...*} var_args 方法的参数.
- */
-baidu.swf.proxy.prototype.call = function(methodName, var_args) {
-    try {
-        var flash = this.getFlash(),
-            args = Array.prototype.slice.call(arguments);
-
-        args.shift();
-        if (flash[methodName]) {
-            flash[methodName].apply(flash, args);
+        } catch (e) {
         }
-    } catch (e) {
     }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* vim: set ts=4 sw=4 sts=4 tw=100 noet: */
+});
