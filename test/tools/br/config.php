04821dd1459a00eef261edc269303957b49df8e9
yangbo <yangbo@BELL-PC.internal.baidu.com>
2011-05-02 12:07:31 +0800

    update for browser setting on 219

--- test/tools/br/config.php
+++ test/tools/br/config.php
@@ -3,9 +3,9 @@ class Config{
 	public static $BROWSERS = array(
 	//'ie6'=>array('10.81.23.218', "C:\\Program Files\\Internet Explorer\\iexplore.exe")
 	//,'maxthon'=>array('10.81.23.218', "C:\\Program Files\\Maxthon3\\Bin\\Maxthon.exe")
-	'firefox'=>array('10.81.23.219', "D:\\Program Files\\mozilla firefox\\firefox.exe")
+	'firefox'=>array('10.81.23.219', "C:\\Program Files\\mozilla firefox\\firefox.exe")
 	,'ie7'=>array('10.81.23.219', "C:\\Program Files\\Internet Explorer\\iexplore.exe")
-	,'chrome'=>array('10.81.23.219', "C:\\Documents and Settings\\Administrator\\Local Settings\\Application Data\\Google\\Chrome\\Application\\chrome.exe")
+	,'chrome'=>array('10.81.23.219', "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")//"C:\\Documents and Settings\\Administrator\\Local Settings\\Application Data\\Google\\Chrome\\Application\\chrome.exe")
 	,'opera'=>array('10.81.23.219', "C:\\Program Files\\Opera\\opera.exe")
 	//,'ie8'=>array('10.81.23.220', "C:\\Program Files\\Internet Explorer\\iexplore.exe")
 	,'safari'=>array('10.81.23.219', "C:\\Program Files\\Safari\\Safari.exe")
