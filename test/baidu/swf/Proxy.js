1f4b5df0f22d3dbf90a28cd4cd4a2cf19eb98eb3
bellcliff <cliff_bell.cn@hotmail.com>
2011-05-06 11:15:55 +0800

    refresh swf.Proxy

--- test/baidu/swf/Proxy.js
+++ test/baidu/swf/Proxy.js
@@ -1,76 +1,40 @@
-/***************************************************************************
- * 
- * Copyright (c) 2011 Baidu.com, Inc. All Rights Reserved
- * $Id$ 
- * 
- **************************************************************************/
+module("baidu.swf.Proxy");
 
-/**
- * proxy.js ~ 2011/01/23 00:32:02
- * 
- * @author leeight(liyubei@baidu.com)
- * @version $Revision$
- * @description
- * 
- */
+test("normal", function() {
+	stop();
+	var isrc = "baidu.swf.create,baidu.ajax.get,baidu.json.decode".split(',');
+	ua.importsrc(isrc.join(','),
+			function() {
+				var div = document.createElement('div');
+				div.id = 'FlashContainer';
+				document.body.appendChild(div);
+				baidu.swf.create({
+					id : "Line",
+					url : upath + "line.swf",
+					width : "100%",
+					height : "165",
+					wmode : "transparent",
+					errorMessage : "载入FLASH出错",
+					ver : "9.0.0",
+					allowscriptaccess : "always"
+				}, "FlashContainer");
 
-//module("baidu.swf.Proxy");
-
-// normal
-//test(
-//		"property&&flash[property] normal",
-//		function() {
-//			stop();
-//			ua
-//					.importsrc(
-//							"baidu.swf.create,baidu.ajax.get,baidu.json.decode",
-//							function() {
-//								var div=document.createElement('div');
-//								div.id='FlashContainer';
-//								document.body.appendChild(div);
-////								$(document.body).append(
-////										"<div id='FlashContainer'></div>");
-//								baidu.swf.create({
-//									id : "Line",
-//									url : upath + "line.swf",
-//									width : "100%",
-//									height : "165",
-//									wmode : "transparent",
-//									errorMessage : "载入FLASH出错",
-//									ver : "9.0.0",
-//									allowscriptaccess : "always"
-//								}, "FlashContainer");
-//
-//								function flashLoaded() {
-//
-//									baidu.ajax
-//											.get(
-//													upath + 'two_line.json',
-//													function(xhr) {
-//														var data = baidu.json
-//																.decode(xhr.responseText);
-//														if (data) {
-//															var isready = proxy
-//																	.isReady();
-//															ok(isready,
-//																	"test isReady() successfully");
-//															proxy
-//																	.call(
-//																			"setFlashLineData",
-//																			data,
-//																			1);
-//															ok(true,
-//																	"test call() and getFlash() successfully");
-//														}
-//														setTimeout(function(){
-//															document.body.removeChild(div);
-//														},200);
-//														start();
-//													});
-//								}
-//
-//								var proxy = new baidu.swf.Proxy("Line",
-//										"setFlashLineData", flashLoaded);
-//								
-//							});
-//		});
+				function flashLoaded() {
+					baidu.ajax.get(upath + 'two_line.json', function(xhr) {
+						var data = baidu.json.decode(xhr.responseText);
+						if (data) {
+							var isready = proxy.isReady();
+							ok(isready, "ready ok");
+							proxy.call("setFlashLineData", data, 1);
+							ok(true, "call ok");
+						}
+						setTimeout(function() {
+							document.body.removeChild(div);
+						}, 200);
+						start();
+					});
+				}
+				var proxy = new baidu.swf.Proxy("Line", "setFlashLineData",
+						flashLoaded);
+			}, isrc[0], 'baidu.swf.Proxy');
+});
