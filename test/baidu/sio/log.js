c9850fd90d7fa6fa4d43f581a568792540ac75e2
bellcliff <cliff_bell.cn@hotmail.com>
2011-05-09 16:26:37 +0800

    sio.log的用例，中间有使用ajax.post，之前的版本写成了sio.post

--- test/baidu/sio/log.js
+++ test/baidu/sio/log.js
@@ -1,26 +1,24 @@
 module("baidu.sio.log");
 
-test("onsuccess",
-		function() {
-			stop();
-			var check = function() {
-				var now = Date.now();
-				var filename = Math.random() +'.txt'; 
-				var arg = "loginfo=test" + "&file=" + filename;
-				var urlstring = upath + 'log.php?' + arg;
-				//
-				baidu.sio.log(urlstring);
-				setTimeout(function() {
-					var urlstring = upath + "logcheck.php";
-					var xhr = baidu.sio.post(urlstring, arg, successAction);
-					function successAction(xhr, text) {
-						 equals(text, "test", "get log info 'test' true");
-						 equals(xhr.responseText, "test", "get log info 'test' true");
-						start();	
-					}	
-				}, 1000);
+test("onsuccess", function() {
+	stop();
+	var check = function() {
+		var now = Date.now();
+		var filename = Math.random() + '.txt';
+		var arg = "loginfo=test" + "&file=" + filename;
+		var urlstring = upath + 'log.php?' + arg;
+		//
+		baidu.sio.log(urlstring);
+		setTimeout(function() {
+			var urlstring = upath + "logcheck.php";
+			var xhr = baidu.ajax.post(urlstring, arg, successAction);
+			function successAction(xhr, text) {
+				equals(text, "test", "get log info 'test' true");
+				equals(xhr.responseText, "test", "get log info 'test' true");
+				start();
+			}
+		}, 1000);
 
-			};
-			ua.importsrc('baidu.sio.post', check, 'baidu.sio.post',
-					'baidu.sio.log');
-		});
+	};
+	ua.importsrc('baidu.ajax.post', check, 'baidu.ajax.post', 'baidu.sio.log');
+});
