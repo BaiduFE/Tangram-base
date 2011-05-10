2a116590f019e9eb8cb6a37794564fdd4500a4b6
bellcliff <cliff_bell.cn@hotmail.com>
2011-05-10 16:49:39 +0800

    update for ci测试机器恢复

--- test/tools/br/config.php
+++ test/tools/br/config.php
@@ -1,14 +1,14 @@
 <?php
 class Config{
 	public static $BROWSERS = array(
-	//'ie6'=>array('10.81.23.218', "C:\\Program Files\\Internet Explorer\\iexplore.exe")
-	//,'maxthon'=>array('10.81.23.218', "C:\\Program Files\\Maxthon3\\Bin\\Maxthon.exe")
-	'firefox'=>array('10.81.23.219', "C:\\Program Files\\mozilla firefox\\firefox.exe")
+	'ie6'=>array('10.81.58.63', "C:\\Program Files\\Internet Explorer\\iexplore.exe")
+	,'maxthon'=>array('10.81.58.63', "C:\\Program Files\\Maxthon3\\Bin\\Maxthon.exe")
+	'firefox'=>array('10.81.58.63', "C:\\Program Files\\mozilla firefox\\firefox.exe")
 	,'ie7'=>array('10.81.23.219', "C:\\Program Files\\Internet Explorer\\iexplore.exe")
 	,'chrome'=>array('10.81.23.219', "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")//"C:\\Documents and Settings\\Administrator\\Local Settings\\Application Data\\Google\\Chrome\\Application\\chrome.exe")
 	,'opera'=>array('10.81.23.219', "C:\\Program Files\\Opera\\opera.exe")
-	//,'ie8'=>array('10.81.23.220', "C:\\Program Files\\Internet Explorer\\iexplore.exe")
-	,'safari'=>array('10.81.23.219', "C:\\Program Files\\Safari\\Safari.exe")
+	,'ie8'=>array('10.81.58.64', "C:\\Program Files\\Internet Explorer\\iexplore.exe")
+	,'safari'=>array('10.81.58.64', "C:\\Program Files\\Safari\\Safari.exe")
 	//	,'360'=>array('10.81.23.220', "C:\\Program Files\\360\\360se3\\360SE.exe")
 	//	, 'baidu'=>array('10.81.21.93', "C:\\Program Files\\baidu\\baidubrowser\\baidubrowser.exe")
 	);
