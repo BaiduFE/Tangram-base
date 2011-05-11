3e741f8f7df53a50cf56620ceda73b52de9a40ea
dailiqi <dailiqi@baidu.com>
2011-05-11 18:38:41 +0800

    针对覆盖率添加case

--- test/baidu/array/some.js
+++ test/baidu/array/some.js
@@ -8,3 +8,12 @@ test("基础校验", function(){
             return true;
     }), true,  "基础校验");
 });
+
+test("基础校验", function(){
+    var source = [1,2,3];
+
+    equals(baidu.array.some(source, function(item, i){
+        if(typeof item == 'string')
+            return false;
+    }), false,  "基础校验");
+});
