9b3ade8f08bc47c6b92f0718f54cb649e261b29e
dailiqi <dailiqi@baidu.com>
2011-05-17 17:25:19 +0800

    swf.create : case调用方法改成同步方法 - [Testcase] 解决swf部分case无法通过问题
    Detail 修改将其改成同步方式，将错误用例version 改成100.0.0

--- test/baidu/swf/create.js
+++ test/baidu/swf/create.js
@@ -2,61 +2,50 @@ module("baidu.swf.create");
 
 // 1、normal
 test("test ver", function() {
-	stop();
 	var div = document.body.appendChild(document.createElement('div'));
 	div.id = "div_id";
 	baidu.swf.create({
 		id : 'test1',
-		url : "short1.swf",
-		width:'100',
-		height:'100',
-		ver:'6.0.0'
+		url : upath + "short1.swf",
+		width : '100',
+		height : '100',
+		ver : '6.0.0'
 	}, div);
-	ok(true, "flash对象创建成功");//这个貌似也没有校验。。回头确认，确认完毕请移除此处的说明
-	setTimeout(function(){
-		document.body.removeChild(div);
-	},2000);
-	start();
+	ok(true, "flash对象创建成功");// 这个貌似也没有校验。。回头确认，确认完毕请移除此处的说明
+	document.body.removeChild(div);
 });
 // 2、 There is no flash player or the version of existed flash is lower
 // than
 // needed&&errorMessage
 test("ver errorMessage", function() {
-	stop();
 	var div = document.body.appendChild(document.createElement('div'));
 	baidu.swf.create({
 		id : 'test1',
-		url : "short1.swf",
-		ver:'11.0.0',
-		width:'200',
-		height:'100',
-		errorMessage:"There is no flash player or the version is too low"
+		url : upath + "short1.swf",
+		ver : '100.0.0',
+		width : '200',
+		height : '100',
+		errorMessage : "There is no flash player or the version is too low"
 	}, div);
-	equal(div.innerHTML,"There is no flash player or the version is too low","test option errorMessage successfully");
-	setTimeout(function(){
-		document.body.removeChild(div);
-	},2000);
-	start();
+	equal(div.innerHTML, "There is no flash player or the version is too low",
+			"test option errorMessage successfully");
+	document.body.removeChild(div);
 });
 // 3、There is no flash player or the version of existed flash is lower
 // than
 // needed&&!errorMessage
 test("ver no errorMessage", function() {
-	stop();
 	var div = document.body.appendChild(document.createElement('div'));
-	div.id="div_id";
+	div.id = "div_id";
 	baidu.swf.create({
 		id : 'test1',
-		url : "short1.swf",
-		width:'100',
-		height:'100',
-		ver:'11.0.0'//,杨搏处理，此处多一个,，麻烦海先注意
+		url : upath + "short1.swf",
+		width : '100',
+		height : '100',
+		ver : '100.0.0'// ,杨搏处理，此处多一个,，麻烦海先注意
 	}, 'div_id');
-	equal(div.innerHTML,'',"flash 对象没有创建成功");
-	setTimeout(function(){
-		document.body.removeChild(div);
-	},2000);
-	start();
+	equal(div.innerHTML, '', "flash 对象没有创建成功");
+	document.body.removeChild(div);
 });
 // 4、There is no container(target)
 // test("options&&!container", function() {
