e1d8522b7055af9ad4afcac8a5c73ec4f8105962
dailiqi <dailiqi@baidu.com>
2011-05-18 17:44:02 +0800

    baidu.swf : 将swf的play方式改成false - [Testcase] 解决swf无法移除问题
    Detail 修改swf禁止播放，使之可移除,修改version的错误版本验证，修改createHTML的参数作用域

--- test/baidu/swf/create.js
+++ test/baidu/swf/create.js
@@ -1,27 +1,36 @@
 module("baidu.swf.create");
-
 // 1、normal
 test("test ver", function() {
+	stop();
 	var div = document.body.appendChild(document.createElement('div'));
 	div.id = "div_id";
-	baidu.swf.create({
-		id : 'test1',
-		url : upath + "short1.swf",
-		width : '100',
-		height : '100',
-		ver : '6.0.0'
-	}, div);
+	baidu.swf.create(
+			{
+				id : 'test1',
+				url : upath + "line.swf" + "?cache="
+						+ Math.floor(Math.random() * 1000),
+				width : '100',
+				height : '100',
+				ver : '6.0.0',
+				play : false
+			}, div);
 	ok(true, "flash对象创建成功");// 这个貌似也没有校验。。回头确认，确认完毕请移除此处的说明
-	document.body.removeChild(div);
+	setTimeout(function() {
+		document.body.removeChild(div);
+		start();
+	}, 500);
+
 });
 // 2、 There is no flash player or the version of existed flash is lower
 // than
 // needed&&errorMessage
 test("ver errorMessage", function() {
+	stop();
 	var div = document.body.appendChild(document.createElement('div'));
 	baidu.swf.create({
 		id : 'test1',
-		url : upath + "short1.swf",
+		url : upath + "short1.swf" + "?cache="
+				+ Math.floor(Math.random() * 1000),
 		ver : '100.0.0',
 		width : '200',
 		height : '100',
@@ -29,23 +38,31 @@ test("ver errorMessage", function() {
 	}, div);
 	equal(div.innerHTML, "There is no flash player or the version is too low",
 			"test option errorMessage successfully");
-	document.body.removeChild(div);
+	setTimeout(function() {
+		document.body.removeChild(div);
+		start();
+	}, 500);
 });
 // 3、There is no flash player or the version of existed flash is lower
 // than
 // needed&&!errorMessage
 test("ver no errorMessage", function() {
+	stop();
 	var div = document.body.appendChild(document.createElement('div'));
 	div.id = "div_id";
 	baidu.swf.create({
 		id : 'test1',
-		url : upath + "short1.swf",
+		url : upath + "short1.swf" + "?cache="
+				+ Math.floor(Math.random() * 1000),
 		width : '100',
 		height : '100',
 		ver : '100.0.0'// ,杨搏处理，此处多一个,，麻烦海先注意
 	}, 'div_id');
 	equal(div.innerHTML, '', "flash 对象没有创建成功");
-	document.body.removeChild(div);
+	setTimeout(function() {
+		document.body.removeChild(div);
+		start();
+	}, 500);
 });
 // 4、There is no container(target)
 // test("options&&!container", function() {
