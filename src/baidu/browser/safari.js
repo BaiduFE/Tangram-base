2a4f73c691a32669d319adaf0d26b1a52d08fb06
lixiaopeng <lixiaopeng@baidu.com>
2011-08-18 14:19:08 +0800

    baidu.browser.safari代码修改
    [Bugfix] 合并冲突代码时删除部分代码，导致程序无法运行
    baidu.browser.safari: 修复代码

--- src/baidu/browser/safari.js
+++ src/baidu/browser/safari.js
@@ -5,11 +5,19 @@
 
 ///import baidu.browser;
 
-/**
- * 判断是否为safari浏览器, 支持ipad
- * @property safari safari版本号
- * @grammar baidu.browser.safari
- * @meta standard
- * @see baidu.browser.ie,baidu.browser.firefox,baidu.browser.opera,baidu.browser.chrome   
-*/
-baidu.browser.safari = /(\d+\.\d)?(?:\.\d)?\s+safari\/?(\d+\.\d+)?/i.test(ua) && !/chrome/i.test(ua) ? + (RegExp['\x241'] || RegExp['\x242']) : 0;
+(function(){
+    var ua = navigator.userAgent;
+    /*
+     * 兼容浏览器为safari或ipad,其中,一段典型的ipad UA 如下:
+     * Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10
+     */
+
+    /**
+     * 判断是否为safari浏览器, 支持ipad
+     * @property safari safari版本号
+     * @grammar baidu.browser.safari
+     * @meta standard
+     * @see baidu.browser.ie,baidu.browser.firefox,baidu.browser.opera,baidu.browser.chrome   
+     */
+    baidu.browser.safari = /(\d+\.\d)?(?:\.\d)?\s+safari\/?(\d+\.\d+)?/i.test(ua) && !/chrome/i.test(ua) ? + (RegExp['\x241'] || RegExp['\x242']) : 0;
+})();
