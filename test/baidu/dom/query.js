ed86e65242407bcbf75e5612313f796b4fcabe9e
tianlili <tianlili@b000000020050.internal.baidu.com>
2011-08-19 16:28:02 +0800

    还原误改的用例
    [Testcase] 还原误改的用例
    dom.query: 还原误改的用例

--- test/baidu/dom/query.js
+++ test/baidu/dom/query.js
@@ -121,7 +121,7 @@ test('selector', function() {
 	equals(baidu.dom.query('a:not(a:nth-child(odd))')[0], link1, ':not(a:b)');
 	equals(baidu.dom.query('a:not(a:nth-child(odd) + a)')[0], link, ':not(div + p)');
 	equals(baidu.dom.query('a:not(p > a)').length, 0, ':not(div > p)');
-
+	
 	if(!ua.browser['ie']){
 		equals(baidu.dom.query(':root')[0].tagName.toLowerCase(), 'html', ':root');
 		equals(baidu.dom.query('strong:nth-last-child(2) + span')[0], span, 'strong:nth-last-child(2) + span');
