d51e8ef9ee7828d07bb150dcead806871562a836
徐振华 <webooxx@gmail.com>
2011-12-21 13:21:47 +0800

    [DOC] 更新版本号

--- src/baidu.js
+++ src/baidu.js
@@ -20,7 +20,7 @@
  /**
  * @namespace T Tangram七巧板
  * @name T
- * @version 1.5.0
+ * @version 1.5.1
 */
 
 /**
@@ -28,7 +28,7 @@
  * @author: allstar, erik, meizz, berg
  */
 var T,
-    baidu = T = baidu || {version: "1.5.0"}; 
+    baidu = T = baidu || {version: "1.5.1"}; 
 
 //提出guid，防止在与老版本Tangram混用时
 //在下一行错误的修改window[undefined]
