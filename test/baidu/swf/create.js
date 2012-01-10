71c765d214a7e2e41fe27abe3f0afd5f59f5ebd7
tianlili <tianlili@b000000020050.internal.baidu.com>
2012-01-10 15:28:53 +0800

    删除框架中对Tangram1.3.9的引用

--- test/baidu/swf/create.js
+++ test/baidu/swf/create.js
@@ -2,32 +2,34 @@ module("baidu.swf.create");
 
 test('test', function() {
 	stop();
-	var div = document.body.appendChild(document.createElement('div'));
-	baidu.swf.create({
-		id : 'test0',
-		swliveconnect : 'true',
-		play : 'true',
-		width : 100,
-		height : 100,
-		url : cpath + 'flips2.swf?time=' + new Date().getTime()
-	}, div);
-	var swf = TT.swf.getMovie('test0');
-	var h = setInterval(function() {
-		try {
-			if (swf.Playing)
-				clearInterval(h);
-			else if (swf.GetVariable
-					|| TT.swf.getMovie('test0').GetVariable("/:message"))
-				clearInterval(h);
-			else
+	ua.importsrc("baidu.swf.getMovie", function(){
+		var div = document.body.appendChild(document.createElement('div'));
+		baidu.swf.create({
+			id : 'test0',
+			swliveconnect : 'true',
+			play : 'true',
+			width : 100,
+			height : 100,
+			url : cpath + 'flips2.swf?time=' + new Date().getTime()
+		}, div);
+		var swf = baidu.swf.getMovie('test0');
+		var h = setInterval(function() {
+			try {
+				if (swf.Playing)
+					clearInterval(h);
+				else if (swf.GetVariable
+						|| baidu.swf.getMovie('test0').GetVariable("/:message"))
+					clearInterval(h);
+				else
+					return;
+			} catch (e) {
 				return;
-		} catch (e) {
-			return;
-		}
-		equals(swf.GetVariable("/:message"), 'Type something here',
-				'swf create success');
-		swf.StopPlay();
-		TT.e(div).remove();
-		start();
-	}, 100);
+			}
+			equals(swf.GetVariable("/:message"), 'Type something here',
+					'swf create success');
+			swf.StopPlay();
+			$(div).remove();
+			start();
+		}, 100);
+	}, "baidu.swf.getMovie", "baidu.swf.create");
 });
