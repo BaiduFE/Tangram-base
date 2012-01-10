a60743f1f3e5ea7067f3befd7e4e547d3c4a5f44
tianlili <tianlili@b000000020050.internal.baidu.com>
2012-01-10 17:22:55 +0800

    添加针对swf.create的bugfix的用例
    [Testcase]添加针对swf.create的bugfix的用例
    swf.create: 添加不传target的情况的用例

--- test/baidu/swf/create.js
+++ test/baidu/swf/create.js
@@ -33,3 +33,34 @@ test('test', function() {
 		}, 100);
 	}, "baidu.swf.getMovie", "baidu.swf.create");
 });
+
+test('test, no target', function() {
+	stop();
+	baidu.swf.create({
+		id : 'test0',
+		swliveconnect : 'true',
+		play : 'true',
+		width : 100,
+		height : 100,
+		url : cpath + 'flips2.swf?time=' + new Date().getTime()
+	});
+	var swf = baidu.swf.getMovie('test0');
+	var h = setInterval(function() {
+		try {
+			if (swf.Playing)
+				clearInterval(h);
+			else if (swf.GetVariable
+					|| baidu.swf.getMovie('test0').GetVariable("/:message"))
+				clearInterval(h);
+			else
+				return;
+		} catch (e) {
+			return;
+		}
+		equals(swf.GetVariable("/:message"), 'Type something here',
+				'swf create success');
+		swf.StopPlay();
+		$('#test0').remove();
+		start();
+	}, 100);
+});
