fdd6cf5e1b477bdd47c7e73fca8058770f01c418
tianlili <tianlili@b000000020050.internal.baidu.com>
2012-09-07 10:51:33 +0800

    [Testcase]同步修改opacity的用例

--- test/baidu/dom/opacity.js
+++ test/baidu/dom/opacity.js
@@ -8,7 +8,7 @@ test('set form null to 0.5',function(){
     .css('width', '10px').css('backgroundColor','red');
 	baidu.dom.opacity(div, 0.5);
 	if(ua.browser.ie){
-		equals(div.style.filter, "progid:DXImageTransform.Microsoft.Alpha(opacity:50)", "from '' to 0.5");
+		equals(div.style.filter, "progid:DXImageTransform.Microsoft.Alpha(opacity=50)", "from '' to 0.5");
 	} else{
 		equals(div.style.opacity, 0.5, "from '' to 0.5");
 	}
@@ -23,7 +23,7 @@ test('set form 0.1 to 1',function(){
     .css('width', '10px').css('backgroundColor','red').css('opacity', '0.1');
 	baidu.dom.opacity(div, 1);
 	if(ua.browser.ie){
-		equals(div.style.filter, "progid:DXImageTransform.Microsoft.Alpha(opacity:100)", "from 0.1 to 1");
+		equals(div.style.filter, "progid:DXImageTransform.Microsoft.Alpha(opacity=100)", "from 0.1 to 1");
 	} else{
 		equals(div.style.opacity, 1, "from 0.1 to 1");
 	}
