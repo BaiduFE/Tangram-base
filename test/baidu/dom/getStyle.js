d79c8b1fc858854506bed85382fab3bbe0ddd10b
tianlili <tianlili@baidu.com>
2012-09-10 16:58:13 +0800

    Update test/baidu/dom/getStyle.js
    className->class

--- test/baidu/dom/getStyle.js
+++ test/baidu/dom/getStyle.js
@@ -109,10 +109,10 @@ test("get style from css file", function() {
 	var head = document.getElementsByTagName("head").item(0);
 	div.appendChild(p);
 	div.appendChild(img);
-	$(div).attr('className', "content");
-	$(div1).attr('className', 'content');
-	$(img).attr('className', 'content');
-	$(p).attr('className', 'pid');
+	$(div).attr('class', "content");
+	$(div1).attr('class', 'content');
+	$(img).attr('class', 'content');
+	$(p).attr('class', 'pid');
 
 	var handle = function() {
 		/** IE的float属性叫styleFloat，firefox则是cssFloat * */
